469a90607ecf1aef2c881b920748cc84d1a2456d
James Keicher <jamesdkeicher@gmail.com>
2022-01-31 12:26:02 -0800

    castle dropdown change castle image based on dropdown value

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App() {
           */}
           <SkylineDropdown setSkylineId={setSkylineId}/>
           <WaterfrontDropdown setWaterfrontId={setWaterfrontId}/>
+          <CastleDropdown setCastleId={setCastleId}/>
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
