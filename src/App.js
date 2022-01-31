d8174d6259e04e33ff3fdd3792898f57fe33e0d2
James Keicher <jamesdkeicher@gmail.com>
2022-01-31 12:07:06 -0800

    feat: render images based on id's of dropdowns

--- src/App.js
+++ src/App.js
@@ -11,18 +11,29 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('Las Vegas');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [slogans, setSlogans] = useState(['The City of Sin']);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <img className='city' src={`skyline-${skylineId}.jpeg`}></img>
+      <img className='city' src={`waterfront-${waterfrontId}.jpeg`}></img>
+      <img className='city' src={`castle-${castleId}.jpeg`}></img>
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        {`Welcome to beautiful ${cityName}!`}
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput 
+          setCityName={setCityName}
+        />
         <section className='dropdowns'>
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
