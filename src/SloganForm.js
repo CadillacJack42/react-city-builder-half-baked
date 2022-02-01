5c5f7b69209ea530c1e6ae6470d985dcca0f440d
James Keicher <jamesdkeicher@gmail.com>
2022-02-01 09:48:49 -0800

    CI: fix auto-import in sloganForm

--- src/SloganForm.js
+++ src/SloganForm.js
@@ -1,20 +1,12 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 
 export default function SloganForm({ setSlogans, slogans }) {
   const [newSlogan, setNewSlogan] = useState('');
-  // React forms are a pain! 
-  // Track the sloganInput form state with a useState hook
     
   function handleSubmit(e) {
     e.preventDefault();
-
     setSlogans([...slogans, newSlogan]);
     setNewSlogan('');
-
-    // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans. 
-    // Also, this component also takes in a prop called slogans, which is an arry of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
-
-    // set the sloganInput form state to an empty string to reset the form
   }
 
   const handleChange = (e) => {
@@ -23,11 +15,8 @@ export default function SloganForm({ setSlogans, slogans }) {
 
   return (
     <section>
-      {/* on submit, call the handleSubmit function, defined above */}
       <form onSubmit={handleSubmit}>
         Add a slogan to the list!
-        {/* on change, set the sloganInput in state to be the input value */}
-        {/* also, weirdly, set the value of this input to the sloganInput tracked in state to make this a 'controlled' form input */}
         <input required value={newSlogan} onChange={handleChange}/>
         <button>Submit</button>
       </form>
